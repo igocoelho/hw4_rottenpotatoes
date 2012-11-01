4b199d8eee599765c95ac4db7b42f72f6bfc2eeb
Igo Coelho <igocoelho@igocoelho-linux.(none)>
2012-11-01 19:30:29 -0300

    Features adjust.

--- features/support/paths.rb
+++ features/support/paths.rb
@@ -13,8 +13,18 @@ module NavigationHelpers
   def path_to(page_name)
     case page_name
 
-    when /^the home\s?page$/
-      '/'
+    when /^the (RottenPotatoes )?home\s?page$/ then '/movies'
+    when /^the movies page$/ then '/movies'
+
+    when /^the home\s?page$/ then movies_path
+
+    when /^the (edit|details) page for "(.*)"$/
+      movie = Movie.find_by_title($2)
+      $1 == "edit" ? edit_movie_path(movie) : movie_path(movie)
+
+    when /^the Similar Movies page for "(.*)"$/
+      movie = Movie.find_by_title($1)
+      same_director_path(movie)
 
     # Add more mappings here.
     # Here is an example that pulls values out of the Regexp:
